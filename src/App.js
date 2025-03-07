01284b3718a2ad28ecafa3a6c4f252089bae3806
lenhoare <lenhoare77@gmail.com>
2025-03-07 19:21:24 +0000

    update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Some React Len 
+          Congratulations Len and GPT
         </a>
       </header>
     </div>
